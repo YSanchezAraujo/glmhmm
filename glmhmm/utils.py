f5063df3b02cde58a354a0c782b7ef07684b4f86
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2025-06-04 21:50:46 -0400

    Update utils.py

--- glmhmm/utils.py
+++ glmhmm/utils.py
@@ -2,30 +2,25 @@ import jax.numpy as jnp
 from collections import namedtuple
 from jax import jit
 
-def session_info_neural(x_gauss_in_list):
+def compute_boundaries(list_of_array):
     """Computes trial/segment info using jax.numpy."""
-    n_trials = len(x_gauss_in_list)
-    # Use jnp for array and cumsum
-    samples_per_trial = jnp.array([x.shape[0] for x in x_gauss_in_list])
+    n_trials = len(list_of_array)
+    samples_per_trial = jnp.array([x.shape[0] for x in list_of_array])
     last_idx = jnp.cumsum(samples_per_trial)
     first_idx = last_idx - samples_per_trial
 
-    info = namedtuple(
+    ind_info = namedtuple(
         'info', 
         ['n_trials', 'samples_per_trial', 'last_idx', 'first_idx']
         )(n_trials=n_trials, samples_per_trial=samples_per_trial, last_idx=last_idx, first_idx=first_idx)
-    return info
+    
+    return ind_info
 
-def compute_boundaries(list_of_array):
-    samples_per_set = jnp.array([x.shape[0] for x in list_of_array])
-    last_idx = jnp.cumsum(samples_per_set)
-    first_idx = last_idx - samples_per_set
-
-    inds = namedtuples('inds', ['n_trials', 'first', 'last'])(
-        n_trials = samples_per_set, first = first_idx, last = last_idx
-    )
-
-    return inds
+# will prob remove this, computation is already simple
+@jit
+def transform_theta(theta, ref_state_ind):
+    theta_prime = theta - theta[:, None, ref_state_ind]
+    return theta_prime
 
 @jit
 def transform_theta(theta):
